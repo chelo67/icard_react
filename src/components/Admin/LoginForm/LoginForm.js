5f3cc77cd8eae6d71beeb4f8c8a15720e9cd24a4
marcelo <marceloacampora@gmail.com>
2023-02-04 16:46:28 -0300

    clase 30

---
+++ src/components/Admin/LoginForm/LoginForm.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {Button, Form} from "semantic-ui-react"
+import {useFormik} from "formik"
+import * as Yup from "yup"
+import "./LoginForm.scss"
+
+export function LoginForm() {
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (formValue) => {
+            console.log('Login OK');
+            console.log(formValue);
+        }
+    })
+    return (
+        <Form className="login-form-admin" onSubmit={formik.handleSubmit}>
+            <Form.Input 
+                name="email" 
+                placeholder="Correo electronico" 
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.errors.email}
+            />
+            <Form.Input 
+                name="password" 
+                type="password" 
+                placeholder="password" 
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.errors.password}
+            />
+
+            <Button type="submit" content="iniciar sesion" primary fluid />
+        </Form>
+    )
+}
+
+function initialValues() {
+    return {
+        email: "",
+        password: ""
+    }
+}
+
+function validationSchema() {
+    return {
+        email: Yup.string().email(true).required(true),
+        password: Yup.string().required(true),
+    }
+}
