5c1c7e6b2cabab85a83fd762c2664bee74696042
marcelo <marceloacampora@gmail.com>
2023-02-20 14:06:18 -0300

    seccion 5 completa

--- src/pages/Admin/HomeAdmin.js
+++ src/pages/Admin/HomeAdmin.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {useAuth} from '../../hooks'
 
 export function HomeAdmin() {
+    const {logout} = useAuth();
     return (
         <div>
             <div>Home Admin</div>
+            <button onClick={logout}>Cerrar sesión</button>
         </div>
     )
 }
